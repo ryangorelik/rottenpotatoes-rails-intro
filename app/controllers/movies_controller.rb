5aa04f9f258e008b245067e07d14d8d86652f687
Ryan Gorelik <rgorelik@berkeley.edu>
2020-10-22 20:24:28 +0000

    wip test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,11 @@
 class MoviesController < ApplicationController
-
+ 
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
+    @all_ratings = Movie.all_ratings
+    @ratings_to_show = []
   end
 
   def index
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+  @all_ratings = ['G','PG','PG-13','R']
+  def all_ratings
+    @all_ratings
+  end
 end
